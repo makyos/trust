7f0df6845baf33c6fe0d8345a848a2a7d063324f
makyos <makyos@gmail.com>
2019-10-07 23:52:50 +0900

    zzz

--- registry/src/github.com-1ecc6299db9ec823/twitter-stream-0.9.0/src/rt.rs
+++
@@ -1,7 +0,0 @@
-//! Some reexports from `futures` and `tokio` crates.
-
-extern crate tokio;
-
-pub use self::tokio::{run, spawn};
-pub use futures::future::{lazy, poll_fn};
-pub use futures::{Future, Stream};
